5e09377c2b6e7d56b85e6ba8c0a0c20225bf608d
TAATA EARL <fredvuni809@gmail.com>
2022-11-02 17:16:32 +0300

    Adjust font size of products

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -23,7 +23,7 @@ export const StoreItem = ({id,name,price,imgUrl}:StoreItemProps) =>{
             <Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit:"cover" }}/>
             <Card.Body className="d-flex flex-column">
                 <Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">
-                    <span className="fs-2">{name}</span>
+                    <span className="small">{name}</span>
                     <span className="ms-2 text-muted">{FormatCurrency(price)}</span>
                 </Card.Title>
                 <div className="mt-auto">
